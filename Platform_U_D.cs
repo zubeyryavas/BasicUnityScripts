496bf9ef7a6839369440b2e2d1788c334fbfe9d3
Zubeyr Yavas <zubeyryavas@gmail.com>
2021-10-19 22:10:49 +0200

    01_Potato_mode

---
+++ Platform_U_D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_U_D : MonoBehaviour
+{
+    public Transform pos3, pos4;
+    public float speed = 2f;
+    public Transform startPos;
+
+    Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pos3.position)
+        {
+            nextPos = pos4.position;
+        }
+        if (transform.position == pos4.position)
+        {
+            nextPos = pos3.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos3.position, pos4.position);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //for walking and running
+    public float speed = 1f;
+    //for jumping
+    public int jumpForce;
+    private bool isGrounded;
+    //for dash
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private float dashDirection;
+    private bool isDashing;
+    //for trampoline
+    public float upspeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+        dashTime = startDashTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = 2f;
+        else
+            speed = 1f;
+
+        float xDisplacement = Input.GetAxis("Horizontal") * speed;
+        Vector3 displacementVector = new Vector3(xDisplacement, 0, 0);
+        transform.Translate(displacementVector * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        if (dashDirection == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+                dashDirection = 1;
+            else if (Input.GetKeyDown(KeyCode.X))
+                dashDirection = 2;
+        }
+        else
+        {
+            if (dashTime <= 0)
+            {
+                dashDirection = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                if (dashDirection == 1)
+                    rb.velocity = Vector2.left * dashSpeed;
+                else if (dashDirection == 2)
+                    rb.velocity = Vector2.right * dashSpeed;
+            }
+        }
+        if (isGrounded)
+        {
+            upspeed = 100f;
+        }
+
+
+
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Trampoline" && isGrounded == false) {
+            upspeed += 50f;
+            if (upspeed >= 650)
+            {//tarmpoline
+                upspeed = 650;
+            }
+            rb.AddForce(new Vector2(0, upspeed));
+
+        }else if(collision.gameObject.tag == "Horizontal_Platform" && isGrounded == false)
+        {
+            isGrounded = true;
+            transform.parent = collision.gameObject.transform;
+        }
+        else if (collision.gameObject.tag == "Vertical_Platform" && isGrounded == false)
+        {
+            isGrounded = true;
+            transform.parent = collision.gameObject.transform;
+        }
+        else if (collision.rigidbody)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Start")
+        {//to destroy platform after leaving it
+            Destroy(collision.gameObject);
+            isGrounded = false;
+        }else if (collision.gameObject.tag == "Horizontal_Platform")
+        {
+            transform.parent = null;
+        }
+        else if (collision.gameObject.tag == "Vertical_Platform")
+        {
+            transform.parent = null;
+        }
+        else if (collision.rigidbody)
+        {
+            isGrounded = false;
+        }
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject, 2);
+    }
+}
+
