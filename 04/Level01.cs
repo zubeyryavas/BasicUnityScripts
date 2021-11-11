e5019c838d168a88b6d1143fbe75dceb8a4b4074
Zubeyr Yavas <zubeyryavas@gmail.com>
2021-11-11 21:13:24 +0100

    task 4

---
+++ 04/Level01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level01 : MonoBehaviour
+{
+    public GameObject text;
+
+    public void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            text.SetActive(false);
+        }
+    }
+}
---
+++ 04/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+
+public class Menu : MonoBehaviour
+{
+
+    public InputField nickNameInput;
+    public string nickName;
+
+    public Text errorToggle;
+    const string error_message = "Please check this box to play.";
+
+    public Toggle checkbox;
+
+    public void CheckError()
+    {
+        nickName = nickNameInput.text;
+    
+    }
+    public void StartGame()
+    {
+        if (!checkbox.isOn)
+        {
+            errorToggle.text = error_message;
+        }
+        else
+        {
+            SceneManager.LoadScene("Unity04");
+        }
+    }
+   
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+  
+
+
+}
